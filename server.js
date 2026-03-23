08610a45644b5383c475c96ea5848ed50e751fa2
guillermo de Jong <guillermo.de.jong@ziggo.nl>
2026-03-23 11:33:33 +0100

    werkende registreren v1

--- server.js
+++ server.js
@@ -2,26 +2,103 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 const express = require('express')
+const { MongoClient } = require("mongodb")
 const multer = require('multer')
 const engine = require('ejs-mate')
 const path = require('path')
 const bcryptjs = require('bcryptjs')
-const { MongoClient } = require('mongodb')
 
 const app = express()
 
-// CONFIG
+// 1. CONFIGURATIE & MIDDLEWARE
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
-app.engine('ejs', engine) 
+app.engine('ejs', engine)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 const upload = multer({ dest: 'public/uploads/' })
 
 //////////////////////////////
-//        DATABASE          //
+//        PAGINA'S          //
+//////////////////////////////
+
+function toonHome(req, res) {
+    res.render('paginas/index', { 
+        data: { 
+            pagina: { titel: 'Home' },
+            filters: { bestemmingen: [] }
+        } 
+    })
+}
+
+function toonRegistratie(req, res) {
+    res.render('paginas/registreren', { 
+        data: { pagina: { titel: 'Registreren' } } 
+    })
+}
+
+//////////////////////////////
+//     REGISTRATIE LOGICA   //
+//////////////////////////////
+
+async function verwerkRegistratie(req, res) {
+    try {
+        const collectie = db.collection('gebruikers')
+        const gebruiker = req.body
+
+        console.log("Ontvangen data:", gebruiker)
+
+        // wachtwoord hashen
+        const hashed = await bcryptjs.hash(gebruiker.wachtwoord, 10)
+        gebruiker.wachtwoord = hashed
+
+        // foto upload
+        if (req.file) {
+            gebruiker.profielfoto = req.file.filename
+        }
+
+        // geboortedatum samenvoegen
+        gebruiker.geboorteDatum = {
+            dag: gebruiker.geboorteDag,
+            maand: gebruiker.geboorteMaand,
+            jaar: gebruiker.geboorteJaar
+        }
+
+        delete gebruiker.geboorteDag
+        delete gebruiker.geboorteMaand
+        delete gebruiker.geboorteJaar
+
+        // check dubbele email
+        const bestaat = await collectie.findOne({ email: gebruiker.email })
+        if (bestaat) {
+            return res.send("Email bestaat al!")
+        }
+
+        // opslaan
+        await collectie.insertOne(gebruiker)
+
+        console.log("Gebruiker opgeslagen!")
+        res.redirect('/')
+
+    } catch (err) {
+        console.error("Fout:", err)
+        res.send("Registratie mislukt")
+    }
+}
+
+//////////////////////////////
+//        ROUTES            //
+//////////////////////////////
+
+app.get('/', toonHome)
+app.get('/registreren', toonRegistratie)
+
+app.post('/registreren', upload.single('profielfoto'), verwerkRegistratie)
+
+//////////////////////////////
+//   DATABASE & SERVER      //
 //////////////////////////////
 
 const client = new MongoClient(process.env.DB_URI)
@@ -30,7 +107,8 @@ let db
 async function connectDB() {
     try {
         await client.connect()
-        db = client.db("gebruikersDB")
+        db = client.db("project-tech")
+
         console.log("MongoDB verbonden!")
 
         app.listen(3000, () => {
@@ -43,106 +121,3 @@ async function connectDB() {
 }
 
 connectDB()
-
-//////////////////////////////
-//        PAGINA'S          //
-//////////////////////////////
-
-app.get('/', (req, res) => {
-    res.render('paginas/index', { 
-        data: { 
-            pagina: { titel: 'Home' },
-            filters: { bestemmingen: [] }
-        } 
-    })
-})
-
-app.get('/nieuwe-reis', (req, res) => {
-    res.render('paginas/reis-aanmaken', { 
-        data: { pagina: { titel: 'Nieuwe reis aanmaken' } }
-    })
-})
-
-app.get('/inloggen', (req, res) => {
-    res.render('paginas/inloggen', { 
-        data: { pagina: { titel: 'Log in' } }
-    })
-})
-
-app.get('/registreren', (req, res) => {
-    res.render('paginas/registreren', { 
-        data: { pagina: { titel: 'Registreren' } }
-    })
-})
-
-//////////////////////////////
-//       Registreren        //
-//////////////////////////////
-
-async function verwerkRegistratie(req, res) {
-    try {
-        const collectie = db.collection('gebruikers')
-        const gebruiker = req.body
-
-        //VERPLICHTE VELDEN
-        const verplichteVelden = ['voornaam', 'email', 'wachtwoord', 'land', 'geslacht']
-
-        for (let veld of verplichteVelden) {
-            if (!gebruiker[veld] || gebruiker[veld].trim() === '') {
-                return res.send(`${veld} is verplicht`)
-            }
-        }
-
-        // GEBOORTEDATUM
-        if (!gebruiker.geboorteDag || !gebruiker.geboorteMaand || !gebruiker.geboorteJaar) {
-            return res.send("Geboortedatum is verplicht")
-        }
-
-        // WACHTWOORD LENGTE
-        if (gebruiker.wachtwoord.length < 6) {
-            return res.send("Wachtwoord moet minimaal 6 tekens zijn")
-        }
-
-        //HASH
-        const hashed = await bcryptjs.hash(gebruiker.wachtwoord, 10)
-        gebruiker.wachtwoord = hashed
-
-        //FOTO
-        if (req.file) {
-            gebruiker.profielfoto = req.file.filename
-        }
-
-        //GEBOORTEDATUM
-        gebruiker.geboorteDatum = {
-            dag: gebruiker.geboorteDag,
-            maand: gebruiker.geboorteMaand,
-            jaar: gebruiker.geboorteJaar
-        }
-
-        delete gebruiker.geboorteDag
-        delete gebruiker.geboorteMaand
-        delete gebruiker.geboorteJaar
-
-        //EXTRA: dubbele email check
-        const bestaat = await collectie.findOne({ email: gebruiker.email })
-        if (bestaat) {
-            return res.send("Email bestaat al!")
-        }
-
-        //OPSLAAN IN MONGODB
-        await collectie.insertOne(gebruiker)
-
-        console.log("Gebruiker opgeslagen in MongoDB!")
-        res.redirect('/inloggen')
-
-    } catch (err) {
-        console.error("Fout bij registreren:", err)
-        res.send("Registratie mislukt")
-    }
-}
-
-//////////////////////////////
-//        ROUTES            //
-//////////////////////////////
-
-app.post('/registreren', upload.single('profielfoto'), verwerkRegistratie)
