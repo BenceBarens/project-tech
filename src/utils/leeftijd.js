e49a30c122d2b0e94906fafe8bd69245fe5947eb
Loïs <loisa+thusius@gmail.com>
2026-04-03 23:07:54 +0200

    automatische geslacht & leeftijdscheck toegevoegd + filters uitgebreid met type reis

---
+++ src/utils/leeftijd.js
@@ -0,0 +1,18 @@
+function berekenLeeftijd(geboortedatum) {
+    const vandaag = new Date()
+    const geboorte = new Date(geboortedatum)
+
+    let leeftijd = vandaag.getFullYear() - geboorte.getFullYear()
+    const maandVerschil = vandaag.getMonth() - geboorte.getMonth()
+
+    if (
+        maandVerschil < 0 ||
+        (maandVerschil === 0 && vandaag.getDate() < geboorte.getDate ())
+    ) {
+        leeftijd--
+    }
+    return leeftijd
+}
+module.exports = { berekenLeeftijd }
+
+
