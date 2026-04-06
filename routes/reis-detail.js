c0ee19956610cc07cdf08aa23e36b6fc34d54f54
Bence <51375275+BenceBarens@users.noreply.github.com>
2026-04-06 19:27:42 +0200

    Alleen reis eigenaar ziet aanvragen

--- routes/reis-detail.js
+++ routes/reis-detail.js
@@ -1,48 +1,59 @@
-const express = require('express');
-const router = express.Router();
-const { ObjectId } = require('mongodb');
-const { formatteerGroteReizen } = require('../src/utils/reiskaartGrootFormat');
+const express = require('express')
+const router = express.Router()
+const { ObjectId } = require('mongodb')
+const { formatteerGroteReizen } = require('../src/utils/reiskaartGrootFormat')
 
 router.get('/reis/:id', async (req, res) => {
-    const db = req.app.get('db');
-    const id = req.params.id;
+    const db = req.app.get('db')
+    const id = req.params.id
 
     try {
-        const reis = await db.collection('reizen').findOne({ _id: new ObjectId(id) });
+        const reis = await db.collection('reizen').findOne({ _id: new ObjectId(id) })
+        if (!reis) return res.status(404).render('404')
 
-        if (!reis) {
-            return res.status(404).render('404');
-        }
-
-        let organisatorData = null;
-        if (reis.gebruikerId && ObjectId.isValid(reis.gebruikerId)) {
+        // A. Organisator ophalen
+        let organisatorData = null
+        if (reis.gebruikerId) {
             organisatorData = await db.collection('gebruikers').findOne(
                 { _id: new ObjectId(reis.gebruikerId) },
                 { projection: { voornaam: 1, achternaam: 1, profielfoto: 1 } }
-            );
+            )
         }
 
-        let deelnemersData = [];
+        // B. AANVRAGEN ophalen (mensen in reis.gebruikers)
+        let aanvragenData = []
         if (reis.gebruikers && Array.isArray(reis.gebruikers) && reis.gebruikers.length > 0) {
-            const deelnemerIds = reis.gebruikers.map(uid => new ObjectId(uid));
-            deelnemersData = await db.collection('gebruikers').find(
+            const aanvraagIds = reis.gebruikers.map(uid => new ObjectId(uid))
+            aanvragenData = await db.collection('gebruikers').find(
+                { _id: { $in: aanvraagIds } },
+                { projection: { voornaam: 1, achternaam: 1, profielfoto: 1 } }
+            ).toArray()
+        }
+
+        // C. TOEGELATEN DEELNEMERS ophalen (mensen in reis.deelnemers)
+        let toegelatenDeelnemersData = []
+        if (reis.deelnemers && Array.isArray(reis.deelnemers) && reis.deelnemers.length > 0) {
+            const deelnemerIds = reis.deelnemers.map(uid => new ObjectId(uid))
+            toegelatenDeelnemersData = await db.collection('gebruikers').find(
                 { _id: { $in: deelnemerIds } },
                 { projection: { voornaam: 1, achternaam: 1, profielfoto: 1 } }
-            ).toArray();
+            ).toArray()
         }
 
-        let isEigenaar = false;
+        let isEigenaar = false
         if (req.session.gebruiker && reis.gebruikerId) {
-            isEigenaar = reis.gebruikerId.toString() === req.session.gebruiker.id.toString();
+            isEigenaar = reis.gebruikerId.toString() === req.session.gebruiker.id.toString()
         }
 
+        // Voeg beide lijsten toe aan de data
         const volledigeReisData = {
             ...reis,
             organisator: organisatorData || { voornaam: "Onbekende", achternaam: "Reiziger", profielfoto: null },
-            deelnemersLijst: deelnemersData
-        };
+            aanvragenLijst: aanvragenData,         // De 'gebruikers' array
+            deelnemersLijst: toegelatenDeelnemersData // De 'deelnemers' array
+        }
 
-        const geformatteerdeReis = formatteerGroteReizen([volledigeReisData])[0];
+        const geformatteerdeReis = formatteerGroteReizen([volledigeReisData])[0]
 
         res.render('paginas/reis-detail', { 
             data: { 
@@ -51,12 +62,12 @@ router.get('/reis/:id', async (req, res) => {
                 gebruiker: req.session.gebruiker,
                 pagina: { titel: geformatteerdeReis.reisTitel } 
             } 
-        });
+        })
 
     } catch (err) {
-        console.error("Detailpagina fout:", err);
-        res.status(500).send("Er is iets misgegaan bij het ophalen van de reis.");
+        console.error("Detailpagina fout:", err)
+        res.status(500).send("Er is iets misgegaan bij het ophalen van de reis.")
     }
-});
+})
 
-module.exports = router;
+module.exports = router
