be4868c2d2d68e671cd41c4798b5a4b27346ebbe
Bence <51375275+BenceBarens@users.noreply.github.com>
2026-04-06 20:38:55 +0200

    Werking accepteren/weigeren

--- routes/reis-detail.js
+++ routes/reis-detail.js
@@ -9,50 +9,66 @@ router.get('/reis/:id', async (req, res) => {
 
     try {
         const reis = await db.collection('reizen').findOne({ _id: new ObjectId(id) })
-        if (!reis) return res.status(404).render('404')
+
+        if (!reis) {
+            return res.status(404).render('404')
+        }
 
         // A. Organisator ophalen
         let organisatorData = null
-        if (reis.gebruikerId) {
+        if (reis.gebruikerId && ObjectId.isValid(reis.gebruikerId)) {
             organisatorData = await db.collection('gebruikers').findOne(
                 { _id: new ObjectId(reis.gebruikerId) },
                 { projection: { voornaam: 1, achternaam: 1, profielfoto: 1 } }
             )
         }
 
-        // B. AANVRAGEN ophalen (mensen in reis.gebruikers)
+        // B. AANVRAGEN ophalen (mensen in de 'gebruikers' array van de reis)
         let aanvragenData = []
-        if (reis.gebruikers && Array.isArray(reis.gebruikers) && reis.gebruikers.length > 0) {
-            const aanvraagIds = reis.gebruikers.map(uid => new ObjectId(uid))
-            aanvragenData = await db.collection('gebruikers').find(
-                { _id: { $in: aanvraagIds } },
-                { projection: { voornaam: 1, achternaam: 1, profielfoto: 1 } }
-            ).toArray()
+        if (reis.gebruikers && Array.isArray(reis.gebruikers)) {
+            // Filter om er zeker van te zijn dat we geen null-waarden proberen te zoeken
+            const aanvraagIds = reis.gebruikers
+                .filter(uid => uid != null && uid !== "")
+                .map(uid => new ObjectId(uid))
+
+            if (aanvraagIds.length > 0) {
+                aanvragenData = await db.collection('gebruikers').find(
+                    { _id: { $in: aanvraagIds } },
+                    { projection: { voornaam: 1, achternaam: 1, profielfoto: 1 } }
+                ).toArray()
+            }
         }
 
-        // C. TOEGELATEN DEELNEMERS ophalen (mensen in reis.deelnemers)
-        let toegelatenDeelnemersData = []
-        if (reis.deelnemers && Array.isArray(reis.deelnemers) && reis.deelnemers.length > 0) {
-            const deelnemerIds = reis.deelnemers.map(uid => new ObjectId(uid))
-            toegelatenDeelnemersData = await db.collection('gebruikers').find(
-                { _id: { $in: deelnemerIds } },
-                { projection: { voornaam: 1, achternaam: 1, profielfoto: 1 } }
-            ).toArray()
+        // C. DEELNEMERS ophalen (mensen in de 'deelnemers' array van de reis)
+        let deelnemersData = []
+        if (reis.deelnemers && Array.isArray(reis.deelnemers)) {
+            const deelnemerIds = reis.deelnemers
+                .filter(uid => uid != null && uid !== "")
+                .map(uid => new ObjectId(uid))
+
+            if (deelnemerIds.length > 0) {
+                deelnemersData = await db.collection('gebruikers').find(
+                    { _id: { $in: deelnemerIds } },
+                    { projection: { voornaam: 1, achternaam: 1, profielfoto: 1 } }
+                ).toArray()
+            }
         }
 
+        // D. Eigenaar check
         let isEigenaar = false
         if (req.session.gebruiker && reis.gebruikerId) {
             isEigenaar = reis.gebruikerId.toString() === req.session.gebruiker.id.toString()
         }
 
-        // Voeg beide lijsten toe aan de data
+        // Combineer alle data
         const volledigeReisData = {
             ...reis,
             organisator: organisatorData || { voornaam: "Onbekende", achternaam: "Reiziger", profielfoto: null },
-            aanvragenLijst: aanvragenData,         // De 'gebruikers' array
-            deelnemersLijst: toegelatenDeelnemersData // De 'deelnemers' array
+            aanvragenLijst: aanvragenData,      // Voor de host
+            deelnemersLijst: deelnemersData    // Voor iedereen
         }
 
+        // Formatteer (voor paden naar foto's etc.)
         const geformatteerdeReis = formatteerGroteReizen([volledigeReisData])[0]
 
         res.render('paginas/reis-detail', { 
@@ -66,7 +82,7 @@ router.get('/reis/:id', async (req, res) => {
 
     } catch (err) {
         console.error("Detailpagina fout:", err)
-        res.status(500).send("Er is iets misgegaan bij het ophalen van de reis.")
+        res.status(500).send("Er is iets misgegaan.")
     }
 })
 
