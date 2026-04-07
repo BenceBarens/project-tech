9e390e5b1a31d0b3f31caf97420e7a9f14b88c8d
Bence <51375275+BenceBarens@users.noreply.github.com>
2026-04-07 08:59:30 +0200

    Bekijk contactgegevens van medereizigers

--- routes/reis-detail.js
+++ routes/reis-detail.js
@@ -6,6 +6,7 @@ const { formatteerGroteReizen } = require('../src/utils/reiskaartGrootFormat')
 router.get('/reis/:id', async (req, res) => {
     const db = req.app.get('db')
     const id = req.params.id
+    const sessieGebruikerId = req.session.gebruiker?.id
 
     try {
         const reis = await db.collection('reizen').findOne({ _id: new ObjectId(id) })
@@ -14,19 +15,36 @@ router.get('/reis/:id', async (req, res) => {
             return res.status(404).render('404')
         }
 
-        // A. Organisator ophalen
+        // A. Eigenaar check
+        let isEigenaar = false
+        if (sessieGebruikerId && reis.gebruikerId) {
+            isEigenaar = reis.gebruikerId.toString() === sessieGebruikerId.toString()
+        }
+
+        // B. Deelnemer check (Zit de huidige gebruiker in de deelnemerslijst?)
+        const isDeelnemer = reis.deelnemers?.some(uid => uid.toString() === sessieGebruikerId?.toString())
+
+        // C. Mag de bezoeker contactgegevens zien?
+        const magContactZien = isEigenaar || isDeelnemer
+
+        // D. Organisator ophalen (met email/telefoon als dat mag)
         let organisatorData = null
         if (reis.gebruikerId && ObjectId.isValid(reis.gebruikerId)) {
+            const projectie = { voornaam: 1, achternaam: 1, profielfoto: 1 }
+            if (magContactZien) {
+                projectie.email = 1
+                projectie.telefoonNummer = 1
+            }
+
             organisatorData = await db.collection('gebruikers').findOne(
                 { _id: new ObjectId(reis.gebruikerId) },
-                { projection: { voornaam: 1, achternaam: 1, profielfoto: 1 } }
+                { projection: projectie }
             )
         }
 
-        // B. AANVRAGEN ophalen (mensen in de 'gebruikers' array van de reis)
+        // E. AANVRAGEN ophalen (mensen in de 'gebruikers' array - Alleen voor de host)
         let aanvragenData = []
-        if (reis.gebruikers && Array.isArray(reis.gebruikers)) {
-            // Filter om er zeker van te zijn dat we geen null-waarden proberen te zoeken
+        if (isEigenaar && reis.gebruikers && Array.isArray(reis.gebruikers)) {
             const aanvraagIds = reis.gebruikers
                 .filter(uid => uid != null && uid !== "")
                 .map(uid => new ObjectId(uid))
@@ -39,7 +57,7 @@ router.get('/reis/:id', async (req, res) => {
             }
         }
 
-        // C. DEELNEMERS ophalen (mensen in de 'deelnemers' array van de reis)
+        // F. DEELNEMERS ophalen (mensen in de 'deelnemers' array)
         let deelnemersData = []
         if (reis.deelnemers && Array.isArray(reis.deelnemers)) {
             const deelnemerIds = reis.deelnemers
@@ -47,34 +65,34 @@ router.get('/reis/:id', async (req, res) => {
                 .map(uid => new ObjectId(uid))
 
             if (deelnemerIds.length > 0) {
+                const projectie = { voornaam: 1, achternaam: 1, profielfoto: 1 }
+                if (magContactZien) {
+                    projectie.email = 1
+                    projectie.telefoonNummer = 1
+                }
+
                 deelnemersData = await db.collection('gebruikers').find(
                     { _id: { $in: deelnemerIds } },
-                    { projection: { voornaam: 1, achternaam: 1, profielfoto: 1 } }
+                    { projection: projectie }
                 ).toArray()
             }
-        }
-
-        // D. Eigenaar check
-        let isEigenaar = false
-        if (req.session.gebruiker && reis.gebruikerId) {
-            isEigenaar = reis.gebruikerId.toString() === req.session.gebruiker.id.toString()
         }
 
         // Combineer alle data
         const volledigeReisData = {
             ...reis,
             organisator: organisatorData || { voornaam: "Onbekende", achternaam: "Reiziger", profielfoto: null },
-            aanvragenLijst: aanvragenData,      // Voor de host
-            deelnemersLijst: deelnemersData    // Voor iedereen
+            aanvragenLijst: aanvragenData,
+            deelnemersLijst: deelnemersData
         }
 
-        // Formatteer (voor paden naar foto's etc.)
         const geformatteerdeReis = formatteerGroteReizen([volledigeReisData])[0]
 
         res.render('paginas/reis-detail', { 
             data: { 
                 reis: geformatteerdeReis,
                 isEigenaar: isEigenaar,
+                magContactZien: magContactZien, // Doorgegeven aan EJS
                 gebruiker: req.session.gebruiker,
                 pagina: { titel: geformatteerdeReis.reisTitel } 
             } 
